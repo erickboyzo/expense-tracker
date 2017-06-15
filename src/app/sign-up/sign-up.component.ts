9e884b6dd952c8c3d8e0e47a5827de707ced317d
Erick Boyzo <erickb59@gmail.com>
2017-06-15 00:08:09 -0400

    add signUp component

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.less']
+})
+export class SignUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
