343589583cb23cce2712b2dd3107d2a270fe776a
Erick Boyzo <erick.boyzo@ihg.com>
2017-06-14 20:05:48 -0400

    add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { MaterialModule } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
-
-
 import { ViewLoggedExpensesComponent } from './view-logged-expenses/view-logged-expenses.component';
 import { LogExpenseComponent } from './log-expense/log-expense.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
   { path: 'view-expenses', component: ViewLoggedExpensesComponent },
@@ -24,7 +23,8 @@ const appRoutes: Routes = [
     ViewLoggedExpensesComponent,
     LogExpenseComponent,
     AppHeaderComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
