17bdd2a63203666760ee49ed94e5cd2301eb22a7
Erick Boyzo <erick.boyzo@ihg.com>
2017-06-15 20:20:28 -0400

    add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ViewLoggedExpensesComponent } from './view-logged-expenses/view-logged-expenses.component';
@@ -11,23 +11,25 @@ import { LogExpenseComponent } from './log-expense/log-expense.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
 import { LoginComponent } from './login/login.component';
-
-import { AngularFireModule } from 'angularfire2';
-
-// New imports to update based on AngularFire2 version 4
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import {HttpModule} from "@angular/http";
-import {FormsModule} from "@angular/forms";
 import { SignUpComponent } from './sign-up/sign-up.component';
 
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+
+import { AuthService } from './providers/auth.service';
+import { HomeComponent } from './home/home.component';
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyAdENYvQg1gI_1pONInILcbQNX_Ji4Bss8",
-  authDomain: "expense-tracker-e0028.firebaseapp.com",
-  databaseURL: "https://expense-tracker-e0028.firebaseio.com",
-  projectId: "expense-tracker-e0028",
-  storageBucket: "expense-tracker-e0028.appspot.com",
-  messagingSenderId: "927113629451"
+  apiKey: 'AIzaSyAdENYvQg1gI_1pONInILcbQNX_Ji4Bss8',
+  authDomain: 'expense-tracker-e0028.firebaseapp.com',
+  databaseURL: 'https://expense-tracker-e0028.firebaseio.com',
+  projectId: 'expense-tracker-e0028',
+  storageBucket: 'expense-tracker-e0028.appspot.com',
+  messagingSenderId: '927113629451'
 };
 
 const appRoutes: Routes = [
@@ -45,20 +47,21 @@ const appRoutes: Routes = [
     AppHeaderComponent,
     AppFooterComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
     BrowserAnimationsModule,
-     RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot(appRoutes),
     FormsModule,
     HttpModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
     AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
