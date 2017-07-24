b4c2527cbd03a6b30b0f4992d68805af563b1211
Erick Boyzo <erickb59@gmail.com>
2017-07-23 22:14:29 -0400

    updated chart summary component

---
+++ src/app/view-logged-expenses/chart-summary/chart-summary.component.ts
@@ -0,0 +1,83 @@
+import {Component, OnInit, Input, OnChanges} from '@angular/core';
+import {Router} from "@angular/router";
+
+
+@Component({
+  selector: 'app-chart-summary',
+  templateUrl: 'chart-summary.component.html',
+  styleUrls: ['chart-summary.component.less']
+})
+export class ChartSummaryComponent implements OnInit, OnChanges {
+  @Input() data: any;
+  chart: any;
+  options = {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: 'All expenses up till ' + new Date().toDateString().slice(0, 15)
+    },
+    legend: {
+      align: "center",
+      padding: 10,
+      title: {
+        text: 'Click on category to hide/show'
+      }
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}% (${point.y:.2f})</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: false
+        },
+        showInLegend: true
+      }
+    },
+    series: [
+      {
+        name: 'Category',
+        colorByPoint: true,
+        data: this.data
+      }]
+  };
+
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnChanges(changes: any) {
+    if (!changes.data.firstChange) {
+      if (this.chart) {
+        this.chart.series[0].setData(changes.data.currentValue);
+      }
+    }
+  }
+
+  saveInstance(chartInstance: any) {
+    this.chart = chartInstance;
+    this.chart.series[0].setData(this.data);
+  }
+
+  isDataEmpty(): boolean {
+    return this.data.length === 0;
+  }
+
+  chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  chartHovered(e: any): void {
+    console.log(e);
+  }
+
+}
