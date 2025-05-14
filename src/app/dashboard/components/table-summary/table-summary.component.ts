6f878be9e376463925336daf80858f380c9e3118
Erick Boyzo <erick.boyzo@gmail.com>
2025-05-13 23:42:09 -0400

    feat(dashboard): add category and type filters to table-summary.component.ts

--- src/app/dashboard/components/table-summary/table-summary.component.ts
+++ src/app/dashboard/components/table-summary/table-summary.component.ts
@@ -1,21 +1,26 @@
 import { SelectionModel } from '@angular/cdk/collections';
-import { CurrencyPipe, DatePipe, NgClass } from '@angular/common';
+import { CurrencyPipe, DatePipe, NgClass, NgForOf } from '@angular/common';
 import {
   AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  computed,
+  effect,
   inject,
   Input,
-  OnChanges,
   OnInit,
-  SimpleChanges,
+  signal,
   ViewChild,
+  WritableSignal,
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatCheckbox } from '@angular/material/checkbox';
-import { MatChip } from '@angular/material/chips';
+import { MatChip, MatChipListbox, MatChipOption } from '@angular/material/chips';
 import { MatDialog } from '@angular/material/dialog';
+import { MatDivider } from '@angular/material/divider';
+import { MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
@@ -23,9 +28,10 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
-import { Expense } from '../../../core/interfaces/expense-model';
-import { BulkEditModalComponent } from '../../../shared/components/bulk-edit-modal/bulk-edit-modal.component';
-import { ManageExpenseComponent } from '../../../shared/components/manage-expense/manage-expense.component';
+import { Expense } from '@core/interfaces/expense-model';
+import { BulkEditModalComponent } from '@shared/components/bulk-edit-modal/bulk-edit-modal.component';
+import { ManageExpenseComponent } from '@shared/components/manage-expense/manage-expense.component';
+import { startWith } from 'rxjs';
 import { BulkEditDialogResult } from '../../interfaces/bulk-edit-dialog-data';
 
 @Component({
@@ -46,31 +52,78 @@ import { BulkEditDialogResult } from '../../interfaces/bulk-edit-dialog-data';
     MatSortModule,
     MatPaginatorModule,
     CurrencyPipe,
+    MatChipListbox,
+    MatChipOption,
+    NgForOf,
+    ReactiveFormsModule,
+    MatLabel,
+    MatDivider,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['table-summary.component.scss'],
 })
-export class TableSummaryComponent implements OnInit, OnChanges, AfterViewInit {
+export class TableSummaryComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
   @ViewChild('sorter', { static: true }) sort!: MatSort;
-  @Input() data: Expense[] = [];
+  @Input() data: WritableSignal<Expense[]> = signal([]);
   @Input() displayColumns: string[] = ['select', 'name', 'amount', 'date', 'category', 'type', 'details'];
 
   expensesData = new MatTableDataSource<Expense>();
   selection = new SelectionModel<Expense>(true, []);
   isAllSelected = false;
   category = [];
+  showFilters = signal(false);
+  selectedFilters = new FormControl<string[]>([]);
+  selectedSourceFilters = new FormControl<string[]>([]);
+  filterCategories = computed(() => {
+    const categories = this.data().map((expense) => expense.category);
+    return [...new Set(categories)];
+  });
+  filterSources = computed(() => {
+    const sources = this.data().map((expense) => expense.type);
+    return [...new Set(sources)];
+  });
+  readonly categoryFilterValues = toSignal(
+    this.selectedFilters.valueChanges.pipe(startWith(this.selectedFilters.value || [])),
+    { initialValue: [] as string[] },
+  );
+  readonly sourceFilterValues = toSignal(
+    this.selectedSourceFilters.valueChanges.pipe(startWith(this.selectedSourceFilters.value || [])),
+    { initialValue: [] as string[] },
+  );
+  filteredData = computed(() => {
+    const data = this.data();
+    const categoryFilters = this.categoryFilterValues();
+    const sourceFilters = this.sourceFilterValues();
+
+    if (!categoryFilters?.length && !sourceFilters?.length) {
+      return data;
+    }
+
+    return data.filter((expense) => {
+      const matchesCategory = !categoryFilters?.length || categoryFilters.includes(expense.category);
+      const matchesSource = !sourceFilters?.length || sourceFilters.includes(expense.type);
+      return matchesCategory && matchesSource;
+    });
+  });
 
   private snackBar = inject(MatSnackBar);
+  private init = false;
+  private dataEffect = effect(() => {
+    if (!this.init) return;
+    this.resetFilters();
+    this.updateTableData(this.data());
+  });
+  private filterEffect = effect(() => {
+    const filteredExpenses = this.filteredData();
+    this.updateTableData(filteredExpenses);
+  });
 
-  constructor(public dialog: MatDialog) {}
+  private dialog = inject(MatDialog);
 
   ngOnInit() {
-    this.expensesData = new MatTableDataSource<Expense>(this.data);
-    this.sort.active = 'date';
-    this.sort.direction = 'asc';
-    this.expensesData.paginator = this.paginator;
-    this.expensesData.sort = this.sort;
+    this.updateTableData(this.data());
+    this.init = true;
   }
 
   toggleHeaderCheckbox() {
@@ -98,20 +151,9 @@ export class TableSummaryComponent implements OnInit, OnChanges, AfterViewInit {
     this.expensesData.sort = this.sort;
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (!changes['data'].firstChange) {
-      this.expensesData = new MatTableDataSource<Expense>(this.data);
-      this.expensesData.paginator = this.paginator;
-      this.expensesData.sort = this.sort;
-    }
-  }
-
-  isDataEmpty(): boolean {
-    return this.data.length === 0;
-  }
-
   editData(expense: Expense) {
     const dialogRef = this.dialog.open(ManageExpenseComponent, {
+      width: '600px',
       data: { expense },
       hasBackdrop: true,
       disableClose: true,
@@ -153,5 +195,23 @@ export class TableSummaryComponent implements OnInit, OnChanges, AfterViewInit {
         });
       }
     });
+  }
+
+  toggleFilters() {
+    this.showFilters.update((value) => !value);
+  }
+
+  private updateTableData(data: Expense[]) {
+    this.expensesData = new MatTableDataSource<Expense>(data);
+    this.sort.active = 'date';
+    this.sort.direction = 'asc';
+    this.expensesData.paginator = this.paginator;
+    this.expensesData.sort = this.sort;
+    this.selection.clear();
+  }
+
+  private resetFilters() {
+    this.selectedFilters.reset();
+    this.selectedSourceFilters.reset();
   }
 }
